adddd080c709a76b4ef9df812beba612c76b8e1e
onejibang <onejibang@users.noreply.github.com>
2024-01-28 15:11:26 +0800

    feat: plugin-register-sing-box

---
+++ plugins/GFS/plugin-register-sing-box.js
@@ -0,0 +1,27 @@
+const envStore = Plugins.useEnvStore()
+
+const path = (envStore.env.basePath + "\\" + envStore.env.appName).replaceAll("\\", "\\\\")
+
+const file = 'data/.cache/register_singbox_protocol.reg'
+
+const onInstall = async () => {
+    const reg = `Windows Registry Editor Version 5.00
+[HKEY_CLASSES_ROOT\\sing-box]
+"URL Protocol"="${path}"
+@="${envStore.env.appName}"
+[HKEY_CLASSES_ROOT\\sing-box\\DefaultIcon]
+@="${path},1"
+[HKEY_CLASSES_ROOT\\sing-box\\shell]
+[HKEY_CLASSES_ROOT\\sing-box\\shell\\open]
+[HKEY_CLASSES_ROOT\\sing-box\\shell\\open\\command]
+@="\\"${path}\\" \\"%1\\""`
+
+    await Plugins.Writefile(file, reg)
+    await Plugins.Exec('reg', ['import', file])
+    Plugins.message.info('注册完成')
+}
+
+const onUninstall = async () => {
+    await Plugins.Exec('reg', ['delete', `HKEY_CLASSES_ROOT\\sing-box`, '/f'])
+    Plugins.message.info('卸载完成')
+}
