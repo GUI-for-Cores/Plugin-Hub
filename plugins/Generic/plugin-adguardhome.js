be4cf6715867fd06a4bcdcba05eff1e8d660fc79
Ayideyia <Ayideyia@users.noreply.github.com>
2024-04-06 18:36:03 +0800

    Update adguardhome

--- plugins/Generic/plugin-adguardhome.js
+++ plugins/Generic/plugin-adguardhome.js
@@ -1,3 +1,7 @@
+/**
+ * 本插件使用项目：https://github.com/AdguardTeam/AdGuardHome
+ */
+
 const ADGUARDHOME_PATH = 'data/third/AdGuardHome'
 const PID_FILE = ADGUARDHOME_PATH + '/AdGuardHome.pid'
 const BACKUP_FILE = 'data/third/AdGuardHome.yaml.bak'
@@ -54,7 +58,7 @@ const startAdguardHomeService = async () => {
 const installAdGuardHome = async () => {
   const { env } = Plugins.useEnvStore()
   const tmpZip = 'data/.cache/adguardhome.zip'
-  const url = `https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.45/AdGuardHome_windows_${env.arch}.zip`
+  const url = `https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.48/AdGuardHome_windows_${env.arch}.zip`
   const { id } = Plugins.message.info('下载AdGuardHome压缩包')
   try {
     await Plugins.Download(url, tmpZip, {}, (progress, total) => {
