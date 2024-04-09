732284246584deb98e2b8b3f100d4d4157dc73a2
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2024-04-09 14:24:57 +0800

    feat: plugin-sync-configuration

--- plugins/Generic/plugin-sync-configuration.js
+++ plugins/Generic/plugin-sync-configuration.js
@@ -1,36 +1,66 @@
 const onRun = async () => {
-  console.log(Plugin)
+  const action = await Plugins.picker.single(
+    '请选择操作',
+    [
+      { label: '立即备份', value: 'backup' },
+      { label: '同步至本地', value: 'sync' },
+      { label: '查看备份列表', value: 'list' },
+      { label: '管理备份列表', value: 'remove' }
+    ],
+    []
+  )
+
+  const handler = {
+    backup: Backup,
+    sync: Sync,
+    list: List,
+    remove: Remove
+  }
+
+  await handler[action]()
 }
 
 /**
  * 插件钩子：右键 - 同步至本地
  */
 const Sync = async () => {
-  // const list = await httpGet('/backup')
-  const list = [1]
+  const list = await httpGet('/backup')
   if (list.length === 0) throw '没有可同步的备份'
-  const time = await Plugins.picker.single(
+  const backupId = await Plugins.picker.single(
     '请选择要同步至本地的备份',
-    [
-      { label: '2024/4/9 1:04:40', value: 1 },
-      { label: '2024/4/9 1:04:52', value: 2 },
-      { label: '2024/4/9 1:04:55', value: 3 }
-    ],
-    [list.pop()]
+    list.reverse().map((v) => {
+      const date = new Date(Number(v))
+      const YYYY = date.getFullYear()
+      const MM = date.getMonth() + 1
+      const DD = date.getDate()
+      const hh = date.getHours()
+      const mm = date.getMinutes()
+      const ss = date.getSeconds()
+      return { label: `${YYYY}/${MM}/${DD} ${hh}:${mm}:${ss}`, value: v }
+    }),
+    [list.shift()]
   )
 
-  // const files = await httpGet('/backup?time=' + time)
-  const files = [1]
+  const files = await httpGet('/backup?id=' + backupId)
+
   const { id } = Plugins.message.info('正在同步...', 60 * 60 * 1000)
   for (let i = 0; i < files.length; i++) {
     const file = files[i]
-    // await Plugins.Download(`http://${Plugin.ServerAddress}${file}`)
     Plugins.message.update(id, `正在同步...[ ${i + 1}/${files.length} ]`)
-    await Plugins.sleep(500)
+    try {
+      const encrypted = await httpGet(`/file?path=${backupId}/${file}`)
+      await Plugins.Writefile(file, decrypt(encrypted))
+    } catch (error) {
+      console.log(error)
+      Plugins.message.update(id, `[${file}] 同步失败`, 'error')
+    } finally {
+      await Plugins.sleep(100)
+    }
   }
 
-  Plugins.message.update(id, '同步完成', 'success')
+  Plugins.message.update(id, '同步完成，即将重载界面', 'success')
   await Plugins.sleep(1500).then(() => Plugins.message.destroy(id))
+  await Plugins.WindowReloadApp()
 }
 
 /**
@@ -39,21 +69,79 @@ const Sync = async () => {
 const Backup = async () => {
   const files = ['data/user.yaml', 'data/profiles.yaml', 'data/subscribes.yaml', 'data/rulesets.yaml', 'data/plugins.yaml', 'data/scheduledtasks.yaml']
 
+  const subscribesStore = Plugins.useSubscribesStore()
+  const pluginsStore = Plugins.usePluginsStore()
+  const rulesetsStore = Plugins.useRulesetsStore()
+
+  const l1 = subscribesStore.subscribes.map((v) => v.path).filter((v) => v.startsWith('data'))
+  const l2 = pluginsStore.plugins.map((v) => v.path).filter((v) => v.startsWith('data'))
+  const l3 = rulesetsStore.rulesets.map((v) => v.path).filter((v) => v.startsWith('data') && (v.endsWith('yaml') || v.endsWith('json')))
+
+  files.push(...l1, ...l2, ...l3)
+
   const { id } = Plugins.message.info('正在备份...', 60 * 60 * 1000)
+  const backupId = Date.now().toString()
   for (let i = 0; i < files.length; i++) {
     const file = files[i]
-    const text = await Plugins.Readfile(file)
-    const encrypted = encrypt(text)
     Plugins.message.update(id, `正在备份...[ ${i + 1}/${files.length} ]`)
-    await httpPost('/backup', {
-      time: new Date().toLocaleString(),
-      text: encrypted
-    })
-    await Plugins.sleep(500)
+    try {
+      const text = await Plugins.Readfile(file)
+      const encrypted = encrypt(text)
+      await httpPost('/file', {
+        id: backupId,
+        file: file,
+        body: encrypted
+      })
+    } catch (error) {
+      console.log(error)
+      Plugins.message.update(id, `[${file}] 备份失败`, 'error')
+    } finally {
+      await Plugins.sleep(100)
+    }
   }
 
   Plugins.message.update(id, '备份完成', 'success')
   await Plugins.sleep(1500).then(() => Plugins.message.destroy(id))
+}
+
+const List = async () => {
+  const list = await httpGet('/backup')
+  if (list.length === 0) throw '备份列表为空'
+  await Plugins.picker.single(
+    '服务器备份列表如下：',
+    list.reverse().map((v) => {
+      const date = new Date(Number(v))
+      const YYYY = date.getFullYear()
+      const MM = date.getMonth() + 1
+      const DD = date.getDate()
+      const hh = date.getHours()
+      const mm = date.getMinutes()
+      const ss = date.getSeconds()
+      return { label: `${YYYY}/${MM}/${DD} ${hh}:${mm}:${ss}`, value: v }
+    }),
+    []
+  )
+}
+
+const Remove = async () => {
+  const list = await httpGet('/backup')
+  if (list.length === 0) throw '没有可管理的备份'
+  const ids = await Plugins.picker.multi(
+    '请勾选要删除的备份',
+    list.reverse().map((v) => {
+      const date = new Date(Number(v))
+      const YYYY = date.getFullYear()
+      const MM = date.getMonth() + 1
+      const DD = date.getDate()
+      const hh = date.getHours()
+      const mm = date.getMinutes()
+      const ss = date.getSeconds()
+      return { label: `${YYYY}/${MM}/${DD} ${hh}:${mm}:${ss}`, value: v }
+    }),
+    []
+  )
+  await httpDelete('/backup?ids=' + ids.join(','))
+  Plugins.message.success('删除成功')
 }
 
 const onInstall = async () => {
@@ -107,21 +195,37 @@ function decrypt(data) {
   return window.CryptoJS.AES.decrypt(data, Plugin.Secret).toString(CryptoJS.enc.Utf8)
 }
 
-function httpGet(url) {
-  return Plugins.HttpGet(`http://${Plugin.ServerAddress}${url}`, {
-    'User-Agent': `GUI.for.Cores/${Plugin.id}`,
-    Authentication: Plugin.Secret
+async function httpGet(url) {
+  if (!Plugin.Authorization) throw '未配置TOKEN'
+  const { body } = await Plugins.HttpGet(`http://${Plugin.ServerAddress}${url}`, {
+    'User-Agent': 'GUI.for.Cores',
+    Authorization: 'Bearer ' + Plugin.Authorization
   })
+  // 因为GUI封装的网络请求没有处理响应码为非200的情况
+  if (typeof body === 'string' && ['Faild', 'Unauthorized', 'Error', 'Method not allowed'].some((v) => body.startsWith(v))) {
+    throw body
+  }
+  return body
 }
 
 function httpPost(url, data) {
+  if (!Plugin.Authorization) throw '未配置TOKEN'
   return Plugins.HttpPost(
     `http://${Plugin.ServerAddress}${url}`,
     {
-      UserAgent: `GUI.for.Cores/${Plugin.id}`,
+      'User-Agent': 'GUI.for.Cores',
       'Content-Type': 'application/json',
-      Authentication: Plugin.Secret
+      Authorization: 'Bearer ' + Plugin.Authorization
     },
     data
   )
+}
+
+function httpDelete(url) {
+  if (!Plugin.Authorization) throw '未配置TOKEN'
+  return Plugins.HttpDelete(`http://${Plugin.ServerAddress}${url}`, {
+    'User-Agent': 'GUI.for.Cores',
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + Plugin.Authorization
+  })
 }
