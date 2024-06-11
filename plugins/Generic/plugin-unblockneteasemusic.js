eb526e2ce889d82a238440f83220926991000de2
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2024-06-11 11:16:45 +0800

    Update plugin-unblockneteasemusic

--- plugins/Generic/plugin-unblockneteasemusic.js
+++ plugins/Generic/plugin-unblockneteasemusic.js
@@ -5,33 +5,29 @@ const MUSIC_PATH = 'data/third/unblock-netease-music'
 const PID_FILE = MUSIC_PATH + '/unblock-netease-music.pid'
 const PROCESS_NAME = 'unblockneteasemusic.exe'
 
-// 1、环境变量太多了就不写入UI了，按需修改
-// 2、配置里只列出了默认启用的音源，如需更多请第42行添加
 const ENV = {
   LOG_LEVEL: 'info', //	日志输出等级。请见〈日志等级〉部分。	LOG_LEVEL=debug  info  error
-  // BLOCK_ADS: 'false' // 屏蔽应用内部分广告
-  ENABLE_FLAC: 'true', // 激活无损音质获取
-  ENABLE_LOCAL_VIP: 'true', // 激活本地黑胶 VIP，可选值：true（等同于 CVIP）、cvip 和 svip
+  BLOCK_ADS: String(Plugin.BLOCK_ADS), // 屏蔽应用内部分广告
+  ENABLE_FLAC: String(Plugin.ENABLE_FLAC), // 激活无损音质获取
+  ENABLE_LOCAL_VIP: String(Plugin.ENABLE_LOCAL_VIP), // 激活本地黑胶 VIP，可选值：true（等同于 CVIP）、cvip 和 svip
   // LOCAL_VIP_UID: '', // 仅对这些 UID 激活本地黑胶 VIP，默认为对全部用户生效 LOCAL_VIP_UID=123456789,1234,123456
   // ENABLE_HTTPDNS: false, // 激活故障的 Netease HTTPDNS 查询（不建议）
-  DISABLE_UPGRADE_CHECK: 'true' // 禁用更新检测
-  // FOLLOW_SOURCE_ORDER: 'true', // 严格按照配置音源的顺序进行查询
+  DISABLE_UPGRADE_CHECK: String(Plugin.DISABLE_UPGRADE_CHECK), // 禁用更新检测
+  FOLLOW_SOURCE_ORDER: 'false', // 严格按照配置音源的顺序进行查询
   // JSON_LOG: 'true' // 输出机器可读的 JSON 记录格式
-  // NO_CACHE: 'true', // 停用 cache
-  // MIN_BR: '320000', //	允许的最低源音质，小于该值将被替换	MIN_BR=320000
-  // SELECT_MAX_BR: 'true', //	选择所有音源中的最高码率替换音频	SELECT_MAX_BR=true
+  NO_CACHE: String(Plugin.NO_CACHE), // 停用 cache
+  MIN_BR: Plugin.MIN_BR, //	允许的最低源音质，小于该值将被替换	MIN_BR=320000
+  SELECT_MAX_BR: String(Plugin.SELECT_MAX_BR), //	选择所有音源中的最高码率替换音频	SELECT_MAX_BR=true
   // LOG_FILE: 'app.log', //	从 Pino 端设置日志输出的文件位置。也可以用 *sh 的输出重导向功能 (node app.js >> app.log) 代替	LOG_FILE=app.log
   // JOOX_COOKIE: '', //	JOOX 音源的 wmid 和 session_key cookie	JOOX_COOKIE="wmid=<your_wmid>; session_key=<your_session_key>"
-  // MIGU_COOKIE: '', //	咪咕音源的 aversionid cookie	MIGU_COOKIE="<your_aversionid>"
-  // QQ_COOKIE: '', //	QQ 音源的 uin 和 qm_keyst cookie	QQ_COOKIE="uin=<your_uin>; qm_keyst=<your_qm_keyst>"
+  MIGU_COOKIE: Plugin.MIGU_COOKIE, //	咪咕音源的 aversionid cookie	MIGU_COOKIE="<your_aversionid>"
+  QQ_COOKIE: Plugin.QQ_COOKIE, //	QQ 音源的 uin 和 qm_keyst cookie	QQ_COOKIE="uin=<your_uin>; qm_keyst=<your_qm_keyst>"
   // YOUTUBE_KEY: '', //	Youtube 音源的 Data API v3 Key	YOUTUBE_KEY="<your_data_api_key>"
   // SIGN_CERT: '', //	自定义证书文件	SIGN_CERT="./server.crt"
   // SIGN_KEY: '', //	自定义密钥文件	SIGN_KEY="./server.key"
-  // SEARCH_ALBUM: 'true', //	在其他音源搜索歌曲时携带专辑名称（默认搜索条件 歌曲名 - 歌手，启用后搜索条件 歌曲名 - 歌手 专辑名）	SEARCH_ALBUM=true
-  // NETEASE_COOKIE: '' //	网易云 Cookie	MUSIC_U=007554xxx
+  SEARCH_ALBUM: 'true', //	在其他音源搜索歌曲时携带专辑名称（默认搜索条件 歌曲名 - 歌手，启用后搜索条件 歌曲名 - 歌手 专辑名）	SEARCH_ALBUM=true
+  NETEASE_COOKIE: Plugin.NETEASE_COOKIE //	网易云 Cookie	MUSIC_U=007554xxx
 }
-
-const Log = (...msg) => console.log(`[${Plugin.name}]`, ...msg)
 
 /**
  * 启动服务
@@ -43,7 +39,7 @@ const startUnblockMusicService = () => {
         MUSIC_PATH + '/' + PROCESS_NAME,
         ['-p', Plugin.Port + ':' + (Number(Plugin.Port) + 1), '-a', '127.0.0.1', '-o', ...Plugin.Source],
         async (out) => {
-          Log(out)
+          console.log(`[${Plugin.name}]`, out)
           if (out.includes('HTTP Server running')) {
             Plugins.Writefile(PID_FILE, pid.toString())
             await switchTo(1) // 切换为代理
@@ -59,7 +55,7 @@ const startUnblockMusicService = () => {
         }
       )
     } catch (error) {
-      reject(error.message || message)
+      reject(error.message || error)
     }
   })
 }
