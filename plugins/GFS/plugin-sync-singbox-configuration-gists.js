45024264ad772fdeac1a9b45eb2d7f7353cb1d7e
caoshenghui <csh0518@live.com>
2024-10-11 15:27:26 +0800

    新增插件-同步singbox配置到指定的gistid

---
+++ plugins/GFS/plugin-sync-singbox-configuration-gists.js
@@ -0,0 +1,44 @@
+const onRun = async () => {
+  await updateGist();
+};
+
+const updateGist = async () => {
+  if (!Plugin.GistId) throw '未配置GIST ID';
+  const { id: messageId } = Plugins.message.info('正在更新 Gist...', 60 * 60 * 1000);
+
+  try {
+    const configJsonContent = await Plugins.Readfile('data/sing-box/config.json');
+    if (!configJsonContent) throw 'config.json 文件不存在';
+
+    const updatedGist = await updateGistFile(Plugin.GistId, configJsonContent);
+    Plugins.message.update(messageId, `Gist 更新成功: ${updatedGist}`, 'success');
+  } catch (error) {
+    Plugins.message.update(messageId, `Gist 更新失败: ${error}`, 'error');
+  } finally {
+    await Plugins.sleep(1500).then(() => Plugins.message.destroy(messageId));
+  }
+};
+
+
+async function updateGistFile(gistId, configJsonContent) {
+  if (!Plugin.Authorization) throw '未配置TOKEN';
+
+  const { body } = await Plugins.HttpPatch(`https://api.github.com/gists/${gistId}`, {
+      'User-Agent': 'GUI.for.Cores',
+      'Content-Type': 'application/json',
+      'X-GitHub-Api-Version': '2022-11-28',
+      Accept: 'application/vnd.github+json',
+      Connection: 'close',
+      Authorization: 'Bearer ' + Plugin.Authorization
+    }, {
+      files: {
+        'config.json': {
+          content: configJsonContent
+        }
+      }
+    });
+  if (body.message) {
+    throw body.message
+  }
+  return 'ok'
+}
