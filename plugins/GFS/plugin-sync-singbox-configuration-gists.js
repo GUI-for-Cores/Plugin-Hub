4fc434e5e88d6f4aa8777c54bd9b08be52f29db9
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2024-10-15 18:43:24 +0800

    chore: format

--- plugins/GFS/plugin-sync-singbox-configuration-gists.js
+++ plugins/GFS/plugin-sync-singbox-configuration-gists.js
@@ -1,42 +1,45 @@
 const onRun = async () => {
-  await updateGist();
-};
+  await updateGist()
+}
 
 const updateGist = async () => {
-  if (!Plugin.GistId) throw '未配置GIST ID';
-  const { id: messageId } = Plugins.message.info('正在更新 Gist...', 60 * 60 * 1000);
+  if (!Plugin.GistId) throw '未配置GIST ID'
+  const { id: messageId } = Plugins.message.info('正在更新 Gist...', 60 * 60 * 1000)
 
   try {
-    const configJsonContent = await Plugins.Readfile('data/sing-box/config.json');
-    if (!configJsonContent) throw 'config.json 文件不存在';
+    const configJsonContent = await Plugins.Readfile('data/sing-box/config.json')
+    if (!configJsonContent) throw 'config.json 文件不存在'
 
-    const updatedGist = await updateGistFile(Plugin.GistId, configJsonContent);
-    Plugins.message.update(messageId, `Gist 更新成功: ${updatedGist}`, 'success');
+    const updatedGist = await updateGistFile(Plugin.GistId, configJsonContent)
+    Plugins.message.update(messageId, `Gist 更新成功: ${updatedGist}`, 'success')
   } catch (error) {
-    Plugins.message.update(messageId, `Gist 更新失败: ${error}`, 'error');
+    Plugins.message.update(messageId, `Gist 更新失败: ${error}`, 'error')
   } finally {
-    await Plugins.sleep(1500).then(() => Plugins.message.destroy(messageId));
+    await Plugins.sleep(1500).then(() => Plugins.message.destroy(messageId))
   }
-};
-
+}
 
 async function updateGistFile(gistId, configJsonContent) {
-  if (!Plugin.Authorization) throw '未配置TOKEN';
+  if (!Plugin.Authorization) throw '未配置TOKEN'
 
-  const { body } = await Plugins.HttpPatch(`https://api.github.com/gists/${gistId}`, {
+  const { body } = await Plugins.HttpPatch(
+    `https://api.github.com/gists/${gistId}`,
+    {
       'User-Agent': 'GUI.for.Cores',
       'Content-Type': 'application/json',
       'X-GitHub-Api-Version': '2022-11-28',
       Accept: 'application/vnd.github+json',
       Connection: 'close',
       Authorization: 'Bearer ' + Plugin.Authorization
-    }, {
+    },
+    {
       files: {
         'config.json': {
           content: configJsonContent
         }
       }
-    });
+    }
+  )
   if (body.message) {
     throw body.message
   }
