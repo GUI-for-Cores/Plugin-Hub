eed6419acbfa8d9cfb0bf51de37f13c18266a35f
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-12-01 20:20:02 +0800

    Update plugin-install-font

--- plugins/Generic/plugin-install-font.js
+++ plugins/Generic/plugin-install-font.js
@@ -2,48 +2,79 @@
  * 本插件使用项目：https://github.com/mozilla/twemoji-colr
  */
 
-const InstallFont = async () => {
-  const { id } = Plugins.message.info('正在下载字体...', 5 * 60 * 1000)
-  const tmpFile = await Plugins.AbsolutePath('data/.cache/Twemoji.Mozilla.ttf')
-  const batFile = 'data/.cache/installFont.bat'
-  try {
-    await Plugins.Download('https://github.com/mozilla/twemoji-colr/releases/download/v0.7.0/Twemoji.Mozilla.ttf', tmpFile, {}, (c, t) => {
-      Plugins.message.update(id, '正在下载字体...' + ((c / t) * 100).toFixed(2) + '%')
-    })
-    Plugins.message.update(id, '下载字体完成', 'success')
-  } catch (err) {
-    console.log(err)
-    Plugins.message.update(id, '下载字体失败', 'error')
-    throw '下载字体失败'
-  } finally {
-    await Plugins.sleep(1000)
-    Plugins.message.destroy(id)
+const SRC_URL = 'https://github.com/mozilla/twemoji-colr/releases/download/v0.7.0/Twemoji.Mozilla.ttf'
+const DST_FILE = 'C:/WINDOWS/Fonts/Twemoji.Mozilla.ttf'
+const TMP_FILE = 'data/.cache/Twemoji.Mozilla.ttf'
+
+/* 触发器 手动触发 */
+const onRun = async () => {
+  const envStore = Plugins.useEnvStore()
+
+  if (envStore.env.os !== 'windows') {
+    throw '不支持非Windows系统'
   }
 
-  const { id: id2 } = Plugins.message.info('正在安装字体', 5 * 60 * 1000)
-  try {
-    await Plugins.WriteFile(
-      batFile,
-      `set "zt=${tmpFile}"\nmshta "javascript:new ActiveXObject('Shell.Application').NameSpace(20).CopyHere('%zt:\\=\\\\%',0x0010);close()"`
-    )
-    await Plugins.Exec(batFile)
-    await Plugins.RemoveFile(batFile)
-    await Plugins.RemoveFile(tmpFile)
-    Plugins.message.update(id2, '安装字体成功，请重启APP', 'success')
-  } catch (err) {
-    console.log(err)
-    Plugins.message.update(id2, '安装字体失败', 'error')
-    throw '安装字体失败'
-  } finally {
-    await Plugins.sleep(1000)
-    Plugins.message.destroy(id2)
+  const exists = await Plugins.FileExists(DST_FILE)
+  if (!exists) {
+    await installFont()
+    if (await Plugins.confirm('提示', '是否立即重启客户端，以便字体生效？')) {
+      await Plugins.RestartApp()
+    }
+    return
+  }
+
+  if (await Plugins.confirm('提示', '检测到本字体已安装，是否卸载？')) {
+    await uninstallFont()
   }
 }
 
-const onInstall = async () => {
-  await InstallFont()
+const installFont = async () => {
+  let downloadOK = true
+  if (!(await Plugins.FileExists(TMP_FILE))) {
+    const { update, success, error, destroy } = Plugins.message.info('正在下载字体...', 5 * 60 * 1000)
+    try {
+      await Plugins.Download(SRC_URL, TMP_FILE, {}, (c, t) => {
+        update('正在下载字体...' + ((c / t) * 100).toFixed(2) + '%')
+      })
+      success('下载完成')
+    } catch (e) {
+      console.log(`[${Plugin.name}]`, e)
+      error(e.message || e)
+      downloadOK = false
+    } finally {
+      await Plugins.sleep(1000)
+      destroy()
+    }
+  }
+
+  if (!downloadOK) return
+
+  await Plugins.CopyFile(TMP_FILE, DST_FILE)
+  await Plugins.Exec('reg', [
+    'add',
+    'HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts',
+    '/v',
+    'Twemoji.Mozilla.ttf',
+    '/t',
+    'REG_SZ',
+    '/d',
+    'Twemoji.Mozilla.ttf',
+    '/f'
+  ])
+  Plugins.message.success('安装完成')
 }
 
-const onUninstall = async () => {
-  Plugins.message.info('暂未支持卸载字体，请手动卸载')
+const uninstallFont = async () => {
+  let uninstallOK = true
+  await Plugins.RemoveFile(DST_FILE).catch((e) => {
+    uninstallOK = false
+    if (e.includes('The process cannot access the file because it is being used by another process.')) {
+      Plugins.alert('提示', '请退出本程序，使用系统字体管理程序进行卸载。')
+    }
+  })
+
+  if (!uninstallOK) return
+
+  await Plugins.Exec('reg', ['delete', 'HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts', '/v', 'Twemoji.Mozilla.ttf', '/f'])
+  Plugins.message.success('卸载完成')
 }
